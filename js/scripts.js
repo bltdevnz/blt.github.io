8676ccdd850f8c0a0a5a02c9fc7ebe2fe733a3d1
bltdevnz <bltdevnz@gmail.com>
2015-04-30 22:21:25 +1200

    major update to theme
    major update to theme appearance

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-$( function() {
-	$( ".wheelcontainer" ).tooltip({ tooltipClass: "custom-tooltip-styling" });	
-	$( ".wheelcontainer" ).tooltip({ position: { my: "left+15 center", at: "right center"}});
-    $( ".wheelcontainer" ).tooltip().tooltip( "open" );
-});
